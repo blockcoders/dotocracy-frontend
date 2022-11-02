e69810431d73d0c00258e5ab90558c3c1e90bb0f
Fernando Sirni <fsirni@gmail.com>
2022-11-02 18:53:23 +0100

    fake addresses

--- _mocks/votations-mocks.ts
+++ _mocks/votations-mocks.ts
@@ -1,52 +1,52 @@
 export const votationsMock = [
   {
     name: "Votation",
-    address: "0x1234",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "Votation 2",
-    address: "0x1235",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "This is the number 3",
-    address: "0x1236",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "Votation with long name 4",
-    address: "0x1237",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "Votationwithlongwords 5",
-    address: "0x1238",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "Votation",
-    address: "0x1239",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "Votation 2",
-    address: "0x12310",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "This is the number 3",
-    address: "0x12311",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
     name: "Votation with long name 4",
-    address: "0x12312",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
   {
-    name: "Votationwithlongwords 5",
-    address: "0x12313",
+    name: "Votationwithlongwordsmuchlongerthanbefore 5",
+    address: "5G24svh2w4QXNhsHU5XBxf8N3Sw2MPu7sAemofv1bCuyxAzc",
     endsOn: "2020-12-31 23:59:59",
   },
 ];
