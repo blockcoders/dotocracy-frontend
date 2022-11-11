18471725ccb7798816afe1509da82293df66d967
Fernando Sirni <fsirni@gmail.com>
2022-11-11 19:41:33 +0100

    candidates

--- pages/vote/[address].tsx
+++ pages/vote/[address].tsx
@@ -7,26 +7,72 @@ import { useLoading } from "../../hooks/useLoading";
 import { useContracts } from "../../hooks/useContracts";
 import { useWalletContext } from "../../providers/WalletProvider";
 
+type Proposal = {
+  id: string;
+  name: string;
+  voteStart: number;
+  voteEnd: number;
+  executed: boolean;
+  canceled: boolean;
+  candidates: string[];
+};
+
+type Ballot = {
+  name: string;
+  proposal: Proposal;
+  address: string;
+};
+
 export default function VoteDetail() {
   const { format } = useFormatIntl();
   const router = useRouter();
-  const [candidates, setCandidates] = useState([]);
-  const [ballot, setBallot] = useState({ name: "", endsOn: "" });
+  const [ballot, setBallot] = useState<Ballot>();
   const { startLoading, endLoading, isLoading } = useLoading();
   const { getBallotContractInstance } = useContracts();
   const { state } = useWalletContext();
   const { provider } = state;
   const address = router.query.address as string;
+  const proposalId = router.query.proposalId as string;
 
-  const getBallot = async () => {
+  const getStatus = (proposal: Proposal | undefined) => {
+    if (!proposal) return "";
+    const { executed, canceled } = proposal;
+    let status = canceled ? "canceled" : executed ? "executed" : "active";
+    return format(status);
+  };
+
+  const getProposal = async () => {
     startLoading();
     try {
       if (!address) return;
-      const ballotContract = await getBallotContractInstance(address, provider);
-      //const candidates = await ballotContract.getCandidates();
-      const name = await ballotContract.name();
-      setBallot({ name, endsOn: "" });
-      //setCandidates(candidates);
+      if (!proposalId) return;
+      const ballotContract = await getBallotContractInstance(
+        address,
+        provider?.getSigner()
+      );
+      const ballot = await ballotContract.name();
+      const [name, voteStart, voteEnd, executed, canceled, candidates] =
+        await Promise.all([
+          ballotContract.proposalDescription(proposalId),
+          ballotContract.startsOn(proposalId),
+          ballotContract.endsOn(proposalId),
+          ballotContract.executed(proposalId),
+          ballotContract.canceled(proposalId),
+          ballotContract.options(proposalId),
+        ]);
+      setBallot({
+        name: ballot,
+        address,
+        proposal: {
+          id: proposalId,
+          name,
+          voteStart,
+          voteEnd,
+          executed,
+          canceled,
+          candidates,
+        },
+      });
     } catch (error) {
       console.log(error);
     }
@@ -34,20 +80,28 @@ export default function VoteDetail() {
   };
 
   useEffect(() => {
-    getBallot();
-  }, [address]);
+    getProposal();
+  }, [address, proposalId]);
 
   return (
     <>
+      {isLoading && <Spinner size="md" />}
+
       <Box pl={10} pt={5}>
         <Text fontSize="3xl" fontWeight="bold">
-          {ballot.name}
+          {ballot?.name}
         </Text>
         <Text>
           {format("address")}: {address}
         </Text>
         <Text>
-          {format("ends_on")}: {ballot.endsOn}
+          {format("starts_on")}: {ballot?.proposal.voteStart}
+        </Text>
+        <Text>
+          {format("ends_on")}: {ballot?.proposal.voteEnd}
+        </Text>
+        <Text>
+          {format("status")}: {getStatus(ballot?.proposal)}
         </Text>
       </Box>
 
@@ -68,7 +122,7 @@ export default function VoteDetail() {
           columnGap={6}
           rowGap={10}
         >
-          {candidates.map((c: any, index) => (
+          {ballot?.proposal?.candidates.map((c: any, index) => (
             <Candidate key={index.toString()} {...c} address={address} />
           ))}
         </Grid>
